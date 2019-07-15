fe91559da375fb3979ae798836d81590931ca832
Josh Steverman <jstever@umich.edu>
2019-07-15 15:42:16 -0400

    grep -v htitem not needed to get lists

--- get_item_lists.sh
+++ get_item_lists.sh
@@ -2,7 +2,7 @@ D=$(date +\%Y-\%m-\%d)
 
 # BOIA: 271744772
 bundle exec ruby get_ht_ids_by_auth.rb /l1/govdocs/authority_lists/txt/boia.auths.txt\
-  | sort | uniq | grep -v htitem > data/boia_ht_item_ids.$D.txt 
+  | sort | uniq > data/boia_ht_item_ids.$D.txt 
 # comm -2 -3 data/boia_ht_item_ids.$D.txt data/boia_current_ht_item_ids.$D.txt\
 #  > data/boia_new_ht_item_ids.$D.txt
 # comm -1 -3 data/boia_ht_item_ids.$D.txt data/boia_current_ht_item_ids.$D.txt\
@@ -19,7 +19,7 @@ bundle exec ruby get_ht_ids_by_series.rb 'Foreign Relations' | sort | uniq | gre
  
 # # EPA: 659388570 
 bundle exec ruby get_ht_ids_by_auth.rb /l1/govdocs/authority_lists/txt/epa.auths.txt\
-  | sort | uniq | grep -v htitem > data/epa_ht_item_ids.$D.txt
+  | sort | uniq > data/epa_ht_item_ids.$D.txt
 # comm -2 -3 data/epa_ht_item_ids.$D.txt data/epa_current_ht_item_ids.$D.txt\
 #  > data/epa_new_ht_item_ids.$D.txt
 # comm -1 -3 data/epa_ht_item_ids.$D.txt data/epa_current_ht_item_ids.$D.txt\
@@ -37,7 +37,7 @@ bash get_crc_item_ids.sh
 
 
 # All: 2062901859
-bundle exec ruby get_ht_ids.rb | sort | uniq | grep -v htitem > data/all_ht_item_ids.$D.txt
+bundle exec ruby get_ht_ids.rb | sort | uniq > data/all_ht_item_ids.$D.txt
 # comm -2 -3 data/all_ht_item_ids.$D.txt data/all_current_ht_item_ids.$D.txt\
 #  > data/all_new_ht_item_ids.$D.txt
 # comm -1 -3 data/all_ht_item_ids.$D.txt data/all_current_ht_item_ids.$D.txt\
