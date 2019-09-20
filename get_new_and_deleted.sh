78428dfca0b70e242c49505fb810d393742c57e9
Josh Steverman <jstever@umich.edu>
2019-09-20 13:41:36 -0400

    add Trail to get_new_and_Deleted

--- get_new_and_deleted.sh
+++ get_new_and_deleted.sh
@@ -12,13 +12,11 @@ comm -2 -3 data/frus_ht_item_ids.$D.txt data/frus_current_ht_item_ids.$D.txt\
 comm -1 -3 data/frus_ht_item_ids.$D.txt data/frus_current_ht_item_ids.$D.txt\
  > data/frus_deleted_ht_item_ids.$D.txt
  
- 
 # EPA: 659388570 
 comm -2 -3 data/epa_ht_item_ids.$D.txt data/epa_current_ht_item_ids.$D.txt\
  > data/epa_new_ht_item_ids.$D.txt
 comm -1 -3 data/epa_ht_item_ids.$D.txt data/epa_current_ht_item_ids.$D.txt\
  > data/epa_deleted_ht_item_ids.$D.txt
-
 
 # CRC: 1491645774
 comm -2 -3 data/crc_ht_item_ids.$D.txt data/crc_current_ht_item_ids.$D.txt\
@@ -26,6 +24,11 @@ comm -2 -3 data/crc_ht_item_ids.$D.txt data/crc_current_ht_item_ids.$D.txt\
 comm -1 -3 data/crc_ht_item_ids.$D.txt data/crc_current_ht_item_ids.$D.txt\
  > data/crc_deleted_ht_item_ids.$D.txt
 
+# TRAIL: 1100951433
+comm -2 -3 data/trail_ht_item_ids.$D.txt data/trail_current_ht_item_ids.$D.txt\
+ > data/trail_new_ht_item_ids.$D.txt
+comm -1 -3 data/trail_ht_item_ids.$D.txt data/trail_current_ht_item_ids.$D.txt\
+ > data/trail_deleted_ht_item_ids.$D.txt
 
 # All: 2062901859
 comm -2 -3 data/all_ht_item_ids.$D.txt data/all_current_ht_item_ids.$D.txt\
@@ -39,8 +42,4 @@ comm -2 -3 data/serial_set_ht_item_ids.$D.txt data/serial_set_current_ht_item_id
 comm -1 -3 data/serial_set_ht_item_ids.$D.txt data/serial_set_current_ht_item_ids.$D.txt\
  > data/serial_set_deleted_ht_item_ids.$D.txt
 
-# TRAIL: 1100951433
-comm -2 -3 data/trail_ht_item_ids.$D.txt data/trail_current_ht_item_ids.$D.txt\
- > data/trail_new_ht_item_ids.$D.txt
-comm -1 -3 data/trail_ht_item_ids.$D.txt data/trail_current_ht_item_ids.$D.txt\
- > data/trail_deleted_ht_item_ids.$D.txt
+
